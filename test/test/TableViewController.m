7e68fdd8620902adcca62e9c209eb93944c29321
Olexii Strilets <olexiistrilets@gmail.com>
2017-07-31 10:49:49 +0300

    TST-3: Added table view controller

---
+++ test/test/TableViewController.m
@@ -0,0 +1,50 @@
+//
+//  TableViewController.m
+//  test
+//
+//  Created by Olexii Strilets on 31.07.17.
+//  Copyright © 2017 Strilets. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
+    
+    return cell;
+}
+
+
+@end
